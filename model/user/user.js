ca59bca2126d646bdd4bcad77e7391d26c820c31
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-06-20 16:04:03 -0700

    updated bcrypt

--- model/user/user.js
+++ model/user/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const createError = require('http-errors');
